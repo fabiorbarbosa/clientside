176b2321434219edc1bf9c9e373efee14ef2e40e
Fábio Barbosa <fabio.b@uninove.edu.br>
2016-04-12 13:45:24 -0300

    Implementações

---
+++ AngularJS/SPA/js/configRouter.js
@@ -0,0 +1,13 @@
+'use strict'
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/BuscaLogradouro', {
+        templateUrl : 'templates/BuscaLogradouro.html',
+        controller : 'logradouroController'
+    }).when('/BuscaCEP', {
+        templateUrl : 'templates/BuscaCEP.html',
+        controller : 'cepController'
+    }).otherwise({
+       redirectTo : '/BuscaLogradouro' 
+    });
+}])
